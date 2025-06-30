005b1f34a8e54580c91d74853c489d256605625b
karolinyB <ana.silva@baymetrics.com.br>
2025-06-30 13:38:05 -0300

    chore: add postcss-loader to render tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,13 @@ module.exports = (webpackConfigEnv, argv) => {
   });
 
   return merge(defaultConfig, {
-    // modify the webpack config however you'd like to by adding to this object
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["postcss-loader"],
+        },
+      ],
+    },
   });
 };
