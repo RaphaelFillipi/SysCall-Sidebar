13664e7590afc5db1fa0e5c00c6a03d9f0a34f1b
karolinyB <ana.silva@baymetrics.com.br>
2025-06-30 13:54:14 -0300

    feat: add Sidebar component to root

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,10 @@
-export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+import "./frontend/style/index.css";
+import { Sidebar } from "./frontend/page/Sidebar";
+
+export default function Root() {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
 }
